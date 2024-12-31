ee617f7a2f4d473acd01c01ac90d0e8e2a658a80
Bayu-x3 <rpsmaster12@gmail.com>
2024-12-31 15:38:54 +0700

    feat: create post & validation

--- src/controllers/PostControllers.ts
+++ src/controllers/PostControllers.ts
@@ -1,17 +1,43 @@
 import { Context } from "hono";
+import { z } from "zod";
 
 import prisma from "../../prisma/client";
 
-export const getPost = async (c: Context) => {
-    try{
-        const posts = await prisma.post.findMany({ orderBy: {id: 'desc'}});
+const sendResponse = (c: Context, status: number, success: boolean, message: string, data?: any) => {
+    return c.json({ success, message, data });
+};
 
-        return c.json({
-            success: true,
-            message: 'List Data Post',
-            data: posts
-        }, 200);
+export const getPost = async (c: Context) => {
+    try {
+        const posts = await prisma.post.findMany({ orderBy: { id: 'desc' } });
+        return sendResponse(c, 200, true, 'List Data Post', posts);
     } catch (e) {
-        console.error(`Error getting post': ${e}`);
+        console.error(`Error getting posts: ${e}`);
+        return sendResponse(c, 500, false, 'Failed to fetch posts');
     }
-}
+};
+
+const postSchema = z.object({
+    title: z.string().min(1, 'Title is required'),
+    content: z.string().min(1, 'Content is required'),
+})
+
+export async function createPost(c: Context) {
+    try {
+        const body = await c.req.parseBody();
+        const parseBody = postSchema.parse(body);
+
+        const post = await prisma.post.create({
+            data: parseBody
+        })
+
+        return sendResponse(c, 201, true, 'Post created success', post);
+
+    } catch (e) {
+        console.error(`Error creating post: ${e}`);
+        if (e instanceof z.ZodError) {
+            return sendResponse(c, 400, false, 'Validation error', e.errors);
+        }
+        return sendResponse(c, 500, false, 'Failed to create post');
+    }
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,10 @@
 import { Hono } from "hono";
 
-import { getPost } from "../controllers/PostControllers";
+import { getPost, createPost } from "../controllers/PostControllers";
 
 const router = new Hono();
 
 router.get('/', (c) => getPost(c));
+router.post('/', (c) => createPost(c));
 
 export const Routes = router;
